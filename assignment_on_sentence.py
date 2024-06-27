1d83379288567a134be82769187072d4fa7a3338
Janetyinka09 <jancyalice09@gmail.com>
2024-06-27 10:05:01 -0700

    added a file

---
+++ assignment_on_sentence.py
@@ -0,0 +1,4 @@
+users_sentence = input("please enter any sentence of your choice") 
+first_letter_of_sentence = users_sentence[0] 
+last_letter_of_sentence = users_sentence[-1] 
+print("the first letter is "  + first_letter_of_sentence  + " and the last letter is " + last_letter_of_sentence ) 

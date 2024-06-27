d5f268e2040a0299dcb47e2b7c9a554ca799f352
Janetyinka09 <jancyalice09@gmail.com>
2024-06-27 10:08:36 -0700

    added a file

---
+++ input_assignment.py
@@ -0,0 +1,11 @@
+current_year = int(2024) 
+birth_year = int(input("please enter your birth year")) 
+age = current_year - birth_year
+print(" your current age in year 2024 is " , age )  
+
+
+
+
+
+name = input("Please enter your name")
+print(name [0:-1]) 
